4bb7ecc15cbe89f8758435c929f4b6d990d09ba2
MayVargasF <maytevargasfelix@gmail.com>
2022-06-27 11:16:32 +0200

    js conditionals added

--- js/main.js
+++ js/main.js
@@ -2,15 +2,32 @@
 
 const addKitten = document.querySelector(".js-new-form");
 
-addKitten.classList.remove("collapsed");
+
+if (addKitten.classList.contains('.collapsed')) {
+  addKitten.classList.remove(".collapsed"); 
+} else {
+  addKitten.classList.add(".collapsed");
+}
 
 const kittenCards = document.querySelector (".js-list");
 
 const kittenImage1 = 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg';
 const kittenName1 = 'Anastacio';
-const kittenDesc =
+const kittenDesc1 =
   'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
+
+
 const kittenRace = 'British Shorthair';
+
+const kittenRace1 = '';
+
+let kittenRace1ToDisplay;
+
+if (kittenRace1 === "") {
+  kittenRace1ToDisplay = `No se ha especificado la raza`;
+} else {
+  kittenRace1ToDisplay = kittenRace1;
+}
 
 const kittenOne = `<li class="card">
 <article>
@@ -20,9 +37,9 @@ const kittenOne = `<li class="card">
     alt="gatito"
   />
   <h3 class="card_title">${kittenName1.toUpperCase()}</h3>
-  <h4 class="card_race">${kittenRace}</h4>
+  <h4 class="card_race">${kittenRace1ToDisplay}</h4>
   <p class="card_description">
-   ${kittenDesc.replace("Ruiseño", "Risueño")}
+   ${kittenDesc1.replace("Ruiseño", "Risueño")}
   </p>
 </article>
 </li>`;
@@ -32,6 +49,8 @@ const kittenOne = `<li class="card">
 
 const kittenImage2 = 'https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg';
 const kittenName2 = 'Fiona';
+const kittenDesc2 =
+  'Cariñoso, dormilón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
 
 
 const kittenTwo = `<li class="card">
@@ -43,12 +62,14 @@ const kittenTwo = `<li class="card">
 <h3 class="card_title">${kittenName2.toUpperCase()}</h3>
 <h4 class="card_race">${kittenRace}</h4>
 <p class="card_description">
-  ${kittenDesc}
+  ${kittenDesc2}
 </p>
 </li>`;
 
 const kittenImage3 = 'https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg';
 const kittenName3 = 'Cielo';
+const kittenDesc3 =
+  'Arisco, hiperactivo le guta mucho jugar a la pelota. Es una maravilla acariciarle!';
 
 const kittenThree = `<li class="card">
 <img
@@ -59,11 +80,31 @@ const kittenThree = `<li class="card">
 <h3 class="card_title">${kittenName3.toUpperCase()}</h3>
 <h4 class="card_race">${kittenRace}</h4>
 <p class="card_description">
-${kittenDesc}
+${kittenDesc3}
 </p>
 </li>`;
 
 kittenCards.innerHTML = kittenOne + kittenTwo + kittenThree;
 
+const input_search_desc = document.querySelector('.js_in_search_desc');
+
+// input_search_desc.value = 'hiperactivo';
+
+// const descrSearchText = input_search_desc.value;
+
+// if( kittenDesc1.includes(descrSearchText) ) {
+//   kittenCards.innerHTML = kittenOne;
+//   }
+  
+//   if( kittenDesc2.includes(descrSearchText) ) {
+//     kittenCards.innerHTML = kittenTwo;
+//   }
+  
+//   if( kittenDesc3.includes(descrSearchText) ) {
+//     kittenCards.innerHTML = kittenThree;
+//   }
+
+
+  
 
 
