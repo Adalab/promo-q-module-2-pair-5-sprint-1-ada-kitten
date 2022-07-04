e06799f08b733d679af2451a7ae1cd95b68a00a7
MayVargasF <maytevargasfelix@gmail.com>
2022-07-04 19:44:58 +0200

    object and arrays  exercises added

--- js/main.js
+++ js/main.js
@@ -218,25 +218,25 @@ kittenCards.innerHTML = kittenOne + kittenTwo + kittenThree;
 TODO ESTO EQUIVALE A LO DE ABAJO
 */
 
-function renderKitten(url, desc, name, race) {
-  return`<li class="card">
-<img
-  class="card_img"
-  src=${url}
-  alt="gatito"
-/>
-<h3 class="card_title">${name.toUpperCase()}</h3>
-<h4 class="card_race">${race}</h4>
-<p class="card_description">
-${desc}
-</p>
-</li>`;
+// function renderKitten(url, desc, name, race) {
+//   return`<li class="card">
+// <img
+//   class="card_img"
+//   src=${url}
+//   alt="gatito"
+// />
+// <h3 class="card_title">${name.toUpperCase()}</h3>
+// <h4 class="card_race">${race}</h4>
+// <p class="card_description">
+// ${desc}
+// </p>
+// </li>`;
   
-}
+// }
 
-kittenCards.innerHTML += renderKitten(url1,desc1,name1,race1);
-kittenCards.innerHTML += renderKitten(url2,desc2,name2,race);
-kittenCards.innerHTML += renderKitten(url3,desc3,name3,race);
+// kittenCards.innerHTML += renderKitten(url1,desc1,name1,race1);
+// kittenCards.innerHTML += renderKitten(url2,desc2,name2,race);
+// kittenCards.innerHTML += renderKitten(url3,desc3,name3,race);
 
 
 
@@ -295,3 +295,60 @@ const renderRace = () => {
   return htmlRace;
 
 }
+
+
+
+
+const kittenData_1 = {
+  image: 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg',
+  name: 'Anastacio',
+  desc: 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!',
+  race:'',
+};
+
+const kittenData_2 = {
+  image: 'https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg',
+  name: 'Fiona',
+  desc: 'Cariñoso, dormilón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!',
+  race: 'British Shorthair',
+};
+
+const kittenData_3 = {
+  image: 'https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg',
+  name: 'Cielo',
+  desc: 'Arisco, hiperactivo le guta mucho jugar a la pelota. Es una maravilla acariciarle!',
+  race: 'British Shorthair',
+};
+
+const kittenDataList = [kittenData_1,kittenData_2,kittenData_3]
+
+
+function renderKitten(kittenData) {
+  return`<li class="card">
+<img
+  class="card_img"
+  src=${kittenData.image}
+  alt="gatito"
+/>
+<h3 class="card_title">${kittenData.name.toUpperCase()}</h3>
+<h4 class="card_race">${kittenData.race}</h4>
+<p class="card_description">
+${kittenData.desc}
+</p>
+</li>`;
+
+}
+
+let html = '';
+
+let index = 0;
+
+html += renderKitten (kittenDataList[index]);
+
+index++;
+html += renderKitten (kittenDataList[index]);
+
+index++;
+html += renderKitten (kittenDataList[index]);
+
+kittenCards.innerHTML = html;
